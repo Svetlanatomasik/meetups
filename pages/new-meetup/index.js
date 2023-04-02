b5593bbc16faf414a332fa67563422b168df7589
Svetlana Tomašik <seka.corazon@gmail.com>
2023-04-02 17:14:39 +0200

    adding the new meetup form

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,1 +1,12 @@
 // our-domain.com/new-meetup
+import NewMeetupForm from "../../components/meetups/NewMeetupForm";
+
+function NewMeetupPage() {
+  function addMeetupHandler(eneteredMeetupData) {
+    console.log(eneteredMeetupData);
+  }
+
+  return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
+}
+
+export default NewMeetupPage;
