9389f94ec5560150d1b701c174b7e6d0b63f1d61
Svetlana Tomašik <seka.corazon@gmail.com>
2023-04-02 11:05:42 +0200

    creating dynamic pages with parameters

---
+++ pages/news/[newsId].js
@@ -0,0 +1,9 @@
+// (our-domain.com/news/...anyName)
+
+function DetailPage() {
+    return <h1>The Detail Page</h1>;
+  }
+  
+  export default DetailPage;
+  
+  
--- pages/news/index.js
+++ pages/news/index.js
@@ -1,4 +1,4 @@
-//for requests that reach my domain slash news (our-domain.com/news)
+// (our-domain.com/news)
 
 function NewsPage() {
   return <h1>The News Page</h1>;
