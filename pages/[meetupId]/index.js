5c1f622b3905c786be20baabbf7d97ccdc8a5b54
Svetlana Tomašik <seka.corazon@gmail.com>
2023-04-04 15:02:05 +0200

    getStaticPaths

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -11,4 +11,43 @@ function MeetupDetails() {
   );
 }
 
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: "m1",
+        },
+      },
+      {
+        params: {
+          meetupId: "m2",
+        },
+      },
+    ],
+  };
+}
+
+export async function getStaticProps(context) {
+  // fetch data for a single meetup
+
+  const meetupId = context.params.meetupId;
+
+  console.log(meetupId);
+
+  return {
+    props: {
+      meetupData: {
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg",
+        id: "m1",
+        title: "First Meetup",
+        address: "Adress 56, 12345 NewCity",
+        description: "This is the first meetup!",
+      },
+    },
+  };
+}
+
 export default MeetupDetails;
