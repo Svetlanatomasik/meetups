4c6f750d28c8cdfb2207c01acff84ec6b0062ed4
Svetlana Tomašik <seka.corazon@gmail.com>
2023-04-04 11:06:19 +0200

    custom components & CSS modules

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image="https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg"
+      title="First Meetup"
+      address="Adress 56, 12345 NewCity"
+      description="This is the first meetup!"
+    />
+  );
+}
+
+export default MeetupDetails;
