87c826f53ef726e8ea617c988f47f9788387c932
Svetlana Tomašik <seka.corazon@gmail.com>
2023-04-05 17:40:48 +0200

    "head" metadata

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,22 @@
+import Head from "next/head";
+
 import MeetupList from "../components/meetups/MeetupList";
 import { MongoClient } from "mongodb";
+import { Fragment } from "react";
 
 function HomePage(props) {
-  return <MeetupList meetups={props.meetups} />;
+  return (
+    <Fragment>
+      <Head>
+        <title>React Meetups</title>
+        <meta
+          name="description"
+          content="Browse a huge list of highly active React meetups!"
+        />
+      </Head>
+      <MeetupList meetups={props.meetups} />;
+    </Fragment>
+  );
 }
 
 export async function getStaticProps() {
--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,9 +1,11 @@
 // our-domain.com/new-meetup
+import Head from "next/head";
 import { useRouter } from "next/router";
+import { Fragment } from "react";
 import NewMeetupForm from "../../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
-  const router = useRouter()
+  const router = useRouter();
 
   async function addMeetupHandler(enteredMeetupData) {
     const response = await fetch("/api/new-meetup", {
@@ -18,10 +20,21 @@ function NewMeetupPage() {
 
     console.log(data);
 
-    router.push("/")
+    router.push("/");
   }
 
-  return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
+  return (
+    <Fragment>
+      <Head>
+        <title>Add a New Meetup</title>
+        <meta
+          name="description"
+          content="Add your own meetups and create amazing networking opportunities."
+        />
+      </Head>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />;
+    </Fragment>
+  );
 }
 
 export default NewMeetupPage;
