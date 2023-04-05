c458446a61746a03225b37723dceff264918aa07
Svetlana Tomašik <seka.corazon@gmail.com>
2023-04-05 15:15:53 +0200

    working with MongoDB

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,11 +1,31 @@
 // /api/new-meetup
 // POST /api/new-meetup
 
-function handler(req, res) {
+import { MongoClient } from "mongodb";
+
+async function handler(req, res) {
   if (req.method === "POST") {
     const data = req.body;
 
-    const { title, image, address, description } = data;
+    const client = await MongoClient.connect(
+      "mongodb+srv://admin:admin@cluster0.yfkraop.mongodb.net/?retryWrites=true&w=majority"
+    );
+
+    console.log("client ", client);
+
+    const db = client.db();
+
+    const meetupsCollection = db.collection("meetups");
+
+    console.log("meetupsCollection ", meetupsCollection);
+    console.log("pre resulta ");
+    const result = await meetupsCollection.insertOne(data);
+
+    console.log("result ", result);
+
+    client.close();
+
+    res.status(201).json({ message: "Meetup inserted!" });
   }
 }
 
